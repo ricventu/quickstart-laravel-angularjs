4653aaba994b8c5add365e0815fb65c924f01584
Riccardo Venturini <riccardo.venturini@telefin.it>
2017-12-21 11:25:54 +0100

    handle DATABASE_URL for heroku pgsql

--- config/database.php
+++ config/database.php
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => getenv("DATABASE_URL") ? parse_url(getenv("DATABASE_URL"))["host"] : env('DB_HOST', '127.0.0.1'),
+            'port' => getenv("DATABASE_URL") ? parse_url(getenv("DATABASE_URL"))["port"] : env('DB_PORT', '5432'),
+            'database' => getenv("DATABASE_URL") ? substr(parse_url(getenv("DATABASE_URL"))["path"], 1) : env('DB_DATABASE', 'forge'),
+            'username' => getenv("DATABASE_URL") ? parse_url(getenv("DATABASE_URL"))["user"] : env('DB_USERNAME', 'forge'),
+            'password' => getenv("DATABASE_URL") ? parse_url(getenv("DATABASE_URL"))["pass"] : env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
